93cb8aef884f010740417300d469a4061c26b053
WORKANDPLAY <karlsirkas@outlook.com>
2022-09-14 19:05:42 +0200

    DataType

---
+++ src/DataType.java
@@ -0,0 +1,30 @@
+/**
+ * Examples of Primitive datatype
+ *
+ * @author Karl Sirkas
+ */
+
+public class DataType {
+    int a=1;
+    long longNumber =545456681351L;
+    short shortNumber =-32000;
+    float decimalNumber =2.45f;
+    double doubleNumber =3.456d;
+
+    //Character
+    char alpha = "F";
+
+    //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    /*Multi-Line
+    comment
+     */
+
+    int i; // 0
+    float x; // 0.00f
+    char animal; //
+    boolean isClassTomorrow; // false
+}
+
