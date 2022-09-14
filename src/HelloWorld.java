863af706af8d0bac9add8cd633a70fe0cec9cc3a
WORKANDPLAY <karlsirkas@outlook.com>
2022-09-14 18:22:42 +0200

    Helloworld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,10 @@
+/**
+ * the first hello world program
+ *
+ * @author karl sirkas
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
 }
