f3de602e5d344d7a226cbab1c2f7347a12ada426
WORKANDPLAY <karlsirkas@outlook.com>
2022-09-15 19:59:49 +0200

    homwork

---
+++ src/Homework.java
@@ -0,0 +1,19 @@
+public class Homework {
+    public static void main(String[] args) {
+        int i = 9;
+        int a = 10;
+
+        int result = //your expression here;
+
+        /*
+        /1
+        /2
+        /3
+        /4
+        /5
+        /6
+         */
+
+        sout(result);
+    }
+}
