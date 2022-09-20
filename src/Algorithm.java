95ad146fe4b4ddc8596e7d17a83a4e053a9145eb
WORKANDPLAY <karlsirkas@outlook.com>
2022-09-20 19:12:00 +0200

    Homework2

---
+++ src/Algorithm.java
@@ -0,0 +1,11 @@
+public class Algorithm {
+    public static void main(String[] args) {
+        int a = 7;
+        if (a % 2 == 0) {
+            System.out.println("is even");
+        } else if(a == 7) {
+            System.out.println("is odd");
+
+        }
+    }
+}
---
+++ src/StatementExercises.java
@@ -0,0 +1,24 @@
+import java.time.Year;
+
+public class StatementExercises {
+    public static void main(String[] args) {
+        int i;
+        // solution 1
+        for(i=0; i<=20;i++)
+        if (1 % 2 == 0) {
+            System.out.println("This is even number");
+        } else {
+            System.out.println("this is odd number");
+        }
+    }
+
+   //Leap year
+    int year = 2022;
+
+    if (year % 400 == 0|| (year % 4 == 0 && year % 100 != 0 ) ) {
+        System.out.print(year + "is not a leap year");
+    } else {
+        System.out.print(year+ "is not a leap year");
+    }
+    }
+
