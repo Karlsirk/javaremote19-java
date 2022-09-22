06b36222146af1a91ab2a6e05305834bc961afe1
WORKANDPLAY <karlsirkas@outlook.com>
2022-09-22 19:01:45 +0200

    Homework3

--- src/Array.java
+++ src/Array.java
@@ -42,7 +42,7 @@ public class Array {
         }
 
         //enhanced for-loop
-        for(String fruit: fruits) {
+        for(String fruit: fruits) { //String : String[]  int : int[]
             System.out.println(fruit);
         }
 
@@ -57,8 +57,62 @@ public class Array {
         }
 
         System.out.print(result);
+        System.out.println("");
 
         //Homework4
         //To find smallest in the array (b)
+
+        //Multi-dimensional array
+        //20 array
+        int[][] array2d = new  int[3][3];
+        //Row-1
+        array2d[0][0] = 1;
+        array2d[0][1] = 0;
+        array2d[0][2] = 1;
+        //Row-2
+        array2d[1][0] = 2;
+        array2d[1][1] = 10;
+        array2d[1][2] = 9;
+        //Row-3
+        array2d[2][0] = 4;
+        array2d[2][1] = 9;
+        array2d[2][2] = 8;
+
+        int[][] array2dVer2 ={ {1, 0, 1},{2, 10, 9},{4, 9, 8}};
+
+        for(int x = 0; x < 3;x++) {
+            for(int y = 0; y < 3; y++) {
+                System.out.print(array2d[x][y] + " ");
+            }
+            System.out.println();
+        }
+
+        //3D array = 1D + 2D array
+        //1D => number of tables
+        //2D array => array with rows and columns
+        int[][][] array3d = new int[3][3][3];
+
+        //Table-1
+        //Row-1
+        array3d[0][0][0] = 1;
+        array3d[0][0][1] = 0;
+        array3d[0][0][2] = 1;
+
+        int[][][] array3dVer2 = {
+                                {{1, 0, 1}, {1, 8, 0}, {2, 5, 7}},
+                                {{9, 8, 7}, {5, 8, 9}, {3, 5, 0}},
+                                {{5, 6, 2}, {4, 5, 7}, {2, 6, 8}},
+                                 };
+System.out.println("\n");
+        for (int x= 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                for(int c = 0; c < 3; c++) {
+                System.out.print(array3dVer2[x][y][c] + "\t");
+                }
+
+                System.out.print("\n");
+
+    }
+}
     }
 }
